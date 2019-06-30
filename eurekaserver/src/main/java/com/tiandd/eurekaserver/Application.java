6f6478d4baa59f563ff0d4b4433bdc964e6a5bd0
Admin <2293841237@qq.com>
2019-06-30 12:07:47 +0800

    添加Eureka注册中心、gatway微服务网关组件、openFeign生命式HTTP客户端等模块

---
+++ eurekaserver/src/main/java/com/tiandd/eurekaserver/Application.java
@@ -0,0 +1,15 @@
+package com.tiandd.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringCloudApplication
+@EnableEurekaServer
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ feign/src/main/java/com/tiandd/feign/Application.java
@@ -0,0 +1,14 @@
+package com.tiandd.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringCloudApplication
+@EnableFeignClients
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
