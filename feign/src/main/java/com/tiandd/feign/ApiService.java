b2b953b51a25e44b733052e2f45a663d2af26972
Admin <2293841237@qq.com>
2019-06-30 15:06:39 +0800

    添加熔断器

--- feign/src/main/java/com/tiandd/feign/ApiService.java
+++ feign/src/main/java/com/tiandd/feign/ApiService.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Component
-@FeignClient(value = "eurekaclient")
+@FeignClient(value = "eurekaclient",fallback = ApiServiceError.class)
 public interface ApiService {
     @RequestMapping(value = "/index",method = RequestMethod.GET)
     String index();
---
+++ feign/src/main/java/com/tiandd/feign/ApiServiceError.java
@@ -0,0 +1,11 @@
+package com.tiandd.feign;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApiServiceError implements ApiService {
+    @Override
+    public String index() {
+        return "服务发生故障！";
+    }
+}
